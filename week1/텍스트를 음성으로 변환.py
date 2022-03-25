ad6536c6805066bf88b84dcbcb235958cc9bb399
hamon1 <99328993+hamon1@users.noreply.github.com>
2022-03-25 19:18:52 +0900

    Update 텍스트를 음성으로 변환.py

--- week1/텍스트를 음성으로 변환.py
+++ week1/텍스트를 음성으로 변환.py
@@ -1,1 +1,9 @@
+from gtts import gTTS
+from playsound import playsound
 
+file_path = r'텍스트를 음성으로 변환\나의텍스트.txt'
+with open(file_path, 'rt',encoding='UTF8') as f :
+    read_file = f.read()
+
+tts = gTTS(text=read_file, lang='ko')
+tts.save(r"텍스트를 음성으로 변환\나의텍스트.mp3")
